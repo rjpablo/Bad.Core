775d27b030eed5099bc98c27ecb1f7ae9c3eeeab
rjpablo <rjpablo.secondary@gmail.com>
2022-11-27 17:16:24 +0800

    feat: add exception filter and object to JSON extension

---
+++ Bad.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Bad.Core.Extensions
+{    public static class ObjectExtensions
+    {
+        /// <summary>Serializes the object to a JSON string.</summary>
+        /// <returns>A JSON string representation of the object.</returns>
+        public static string ToJson(this object value)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new List<JsonConverter> { new StringEnumConverter() },
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+
+            return JsonConvert.SerializeObject(value, settings);
+            //return JsonSerializer.Serialize(value, new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, });
+
+        }
+    }
+}
---
+++ Bad.Core/Filters/BadExceptionFilter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Bad.Core.Filters
+{
+    public class BadExceptionFilter : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            HttpStatusCode status = HttpStatusCode.InternalServerError;
+            var exceptionType = context.Exception.GetType();
+
+            if (exceptionType == typeof(UnauthorizedAccessException))
+            {
+                status = HttpStatusCode.Unauthorized;
+            }
+            else if (exceptionType == typeof(NotImplementedException))
+            {
+                status = HttpStatusCode.NotImplemented;
+            }
+            else if (exceptionType == typeof(KeyNotFoundException))
+            {
+                status = HttpStatusCode.NotFound;
+            }
+            else
+            {
+                status = HttpStatusCode.InternalServerError;
+            }
+
+            var result = new JsonResult(
+                new { message = context.Exception.Message, stack = context.Exception.StackTrace },
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+            result.StatusCode = (int)status;
+            context.Result = result;
+            context.ExceptionHandled = true;
+        }
+    }
+}
