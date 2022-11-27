2f2904d076c54559f2d37246dc941efce669e5de
rjpablo <rjpablo.secondary@gmail.com>
2022-11-27 17:17:58 +0800

    feat: add unit of work base class

---
+++ Bad.Core/UnitsOfWork/BaseUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bad.Core.UnitsOfWork
+{
+    public class BaseUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public BaseUnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+        public Task<int> SaveChangesAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
