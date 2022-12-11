4eb125a85f26ea8e983a7bccf22465bd9b7f87ef
rjpablo <rjpablo.secondary@gmail.com>
2022-12-12 01:53:47 +0800

    feat: add ILoggerExtensions

---
+++ Bad.Core/Extensions/ILoggerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Bad.Core.Extensions
+{
+    public static class ILoggerExtensions
+    {
+        public static void LogError<T>(this ILogger<T> logger, Exception e)
+        {
+            logger.LogError(e, e.Message);
+        }
+
+    }
+}
