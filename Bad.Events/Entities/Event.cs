ea38c57fa7ef38ac24a02149f8b411155e8e0f3a
rjpablo <rjpablo.secondary@gmail.com>
2022-12-12 01:54:50 +0800

    feat: add Bad.Events project

---
+++ Bad.Events/Entities/Event.cs
@@ -0,0 +1,20 @@
+﻿using Bad.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Bad.Events.Entities
+{
+    public abstract class Event: BaseEntityModel<long>
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [ForeignKey("PrimaryMedia")]
+        public long? PrimaryMediaId { get; set; }
+        public virtual MultimediaModel PrimaryMedia { get; set; }
+        public string CreatedById { get; set; }
+    }
+}
---
+++ Bad.Events/Entities/EventSchedule.cs
@@ -0,0 +1,16 @@
+﻿using Bad.Core.Models;
+using DataAnnotationsExtensions;
+using System;
+using System.ComponentModel;
+
+namespace Bad.Events.Entities
+{
+    public abstract class EventSchedule : BaseEntityModel<long>
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        [DisplayName("No. of Slots")]
+        [Min(1)]
+        public int NumberOfSlots { get; set; }
+    }
+}
