e79996b1e76d4607fa42c1b1a222b0d8efa706fe
rjpablo <rjpablo.secondary@gmail.com>
2023-01-04 20:27:32 +0800

    TOUR-59 add base Review entity model

---
+++ Bad.Core/Models/BadReviewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Bad.Core.Models
+{
+    public class BadReviewModel : BaseEntityModel<long>
+    {
+        public decimal Rating { get; set; }
+        public string Message { get; set; }
+    }
+}
