e62a3e460096ea038f7e5e0268bfa3da4f1c310b
rjpablo <rjpablo.secondary@gmail.com>
2023-01-08 16:26:51 +0800

    feat: add ILogger extension methods

--- Bad.Core/Extensions/ILoggerExtensions.cs
+++ Bad.Core/Extensions/ILoggerExtensions.cs
@@ -9,6 +9,14 @@ namespace Bad.Core.Extensions
         {
             logger.LogError(e, e.Message);
         }
+        public static void LogError<T>(this ILogger<T> logger, Exception e, string methodName)
+        {
+            logger.LogError(e, $"{methodName} failed. {e.Message}.");
+        }
+        public static void LogError<T>(this ILogger<T> logger, Exception e, string methodName, string errorMessage, object input)
+        {
+            logger.LogError(e, $"{methodName} failed. {errorMessage}. Inputs: {input}");
+        }
 
     }
 }
