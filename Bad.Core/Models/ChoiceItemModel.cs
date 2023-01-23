df9413235c7312a62354a4622231fdf3b4fb1113
rjpablo <rjpablo.secondary@gmail.com>
2023-01-23 15:19:34 +0800

    add ChoiceItemModel and PagedGetRequestInputModel

---
+++ Bad.Core/Models/ChoiceItemModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bad.Core.Models
+{
+    public class ChoiceItemModel<TValue, TType>
+    {
+        /// <summary>
+        /// The text that will be displayed in the list of suggestions
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// The value that will be returned when this choice is selected
+        /// </summary>
+        public TValue Value { get; set; }
+
+        public string IconUrl { get; set; }
+        /// <summary>
+        /// The type of this choice item
+        /// </summary>
+        public TType Type { get; set; }
+
+        public ChoiceItemModel() { }
+
+        public ChoiceItemModel(string text, TValue value, string iconUrl, TType type)
+        {
+            Text = text;
+            Value = value;
+            IconUrl = iconUrl;
+            Type = type;
+        }
+    }
+}
---
+++ Bad.Core/Models/PagedGetRequestInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bad.Core.Models
+{
+    public class PagedGetRequestInputModel
+    {
+        public int PageSize { get; set; } = 10;
+        public int PageNumber { get; set; } = 1;
+    }
+}
